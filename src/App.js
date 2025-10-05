010297f5741100460d532bd4efdf36cc642ccb18
smr <smr20thi@gmail.com>
2025-10-05 19:29:18 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+function App() {
+  return (
+    <div>
+      <h1>Women Safety Route Finder</h1>
+      <p>Your safety route companion.</p>
+    </div>
+  );
+}
+
+export default App;
